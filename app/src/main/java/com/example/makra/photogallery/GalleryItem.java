8e94d3b7cdce23363de55dae27c3dc155548a821
makra <makranczi.d@gmail.com>
2017-12-26 14:55:36 +0100

    Chapter 30

--- app/src/main/java/com/example/makra/photogallery/GalleryItem.java
+++ app/src/main/java/com/example/makra/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.example.makra.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by makra on 2017. 12. 21..
  */
@@ -8,6 +10,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -31,6 +34,22 @@ public class GalleryItem {
 
     public void setUrl(String mUrl) {
         this.mUrl = mUrl;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String mOwner) {
+        this.mOwner = mOwner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 
     @Override
--- app/src/main/java/com/example/makra/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/makra/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.makra.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -156,17 +157,29 @@ public class PhotoGalleryFragment extends VisibleFragment {
         new FetchItemsTask(query).execute();
     }
 
-    private class PhotoHolder extends RecyclerView.ViewHolder {
+    private class PhotoHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ImageView mItemImageView;
+        private GalleryItem mGalleryItem;
 
         public PhotoHolder(View itemView) {
             super(itemView);
 
             mItemImageView = (ImageView) itemView.findViewById(R.id.item_image_view);
+            itemView.setOnClickListener(this);
         }
 
         public void bindDrawable(Drawable drawable) {
             mItemImageView.setImageDrawable(drawable);
+        }
+
+        public void bindGalleryItem(GalleryItem galleryItem) {
+            mGalleryItem = galleryItem;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent i = PhotoPageActivity.newIntent(getActivity(), mGalleryItem.getPhotoPageUri());
+            startActivity(i);
         }
     }
 
@@ -187,6 +200,7 @@ public class PhotoGalleryFragment extends VisibleFragment {
         @Override
         public void onBindViewHolder(PhotoHolder holder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
+            holder.bindGalleryItem(galleryItem);
             Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
             holder.bindDrawable(placeholder);
             mThumbnailDownloader.queueThumbnail(holder, galleryItem.getUrl());
